3fd5b4422722d9739b4b727ecdfe49f88c4c066c
Teague-S <endermanspooky@gmail.com>
2026-03-15 17:51:05 -0400

    feat: add /compare-plans route to main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { InputScreen } from "./app/pages/InputScreen";
 import { CostResultsPage } from "./app/pages/CostResultsPage";
+import { ComparePlans } from "./app/pages/ComparePlans";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
@@ -11,8 +12,8 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       <Routes>
         <Route path="/" element={<InputScreen />} />
         <Route path="/cost-results" element={<CostResultsPage />} />
+        <Route path="/compare-plans" element={<ComparePlans />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
 );
-
